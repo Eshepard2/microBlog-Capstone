78788441f04201cab00b79b36cfe48bb2950d5c5
khanimhumbatovagit <khumbatova@pgh.yearup.org>
2022-12-22 14:17:00 -0500

    add openLogInForm(), openRegisterForm(),register function, registerForm.onsubmit

--- LogIn_Register_Page/landing.js
+++ LogIn_Register_Page/landing.js
@@ -3,22 +3,65 @@
 "use strict";
 
 const loginForm = document.querySelector("#login");
+const registerForm = document.querySelector("#register");
+const toggleBtn = document.querySelector("#btn2");
+const toggleBtn1 = document.querySelector("#btn");
+const image1 = document.querySelector("#img1");
+const image2 = document.querySelector("#img2");
 
 loginForm.onsubmit = function (event) {
-    // Prevent the form from refreshing the page,
-    // as it will do by default when the Submit event is triggered:
-    event.preventDefault();
+  // Prevent the form from refreshing the page,
+  // as it will do by default when the Submit event is triggered:
+  event.preventDefault();
 
-    // We can use loginForm.username (for example) to access
-    // the input element in the form which has the ID of "username".
-    const loginData = {
-        username: loginForm.username.value,
-        password: loginForm.password.value,
-    }
+  // We can use loginForm.username (for example) to access
+  // the input element in the form which has the ID of "username".
+  const loginData = {
+    username: loginForm.username.value,
+    password: loginForm.password.value,
+  };
 
-    // Disables the button after the form has been submitted already:
-    loginForm.loginButton.disabled = true;
+  // Disables the button after the form has been submitted already:
+  loginForm.loginButton.disabled = true;
 
-    // Time to actually process the login using the function from auth.js!
-    login(loginData);
+  // Time to actually process the login using the function from auth.js!
+  login(loginData);
+};
+
+function openLoginForm() {
+  image1.style.display = "block";
+  image2.style.display = "none";
+  toggleBtn1.style.display = "block";
+  toggleBtn.style.display = "none";
+  loginForm.style.display = "block";
+  registerForm.style.display = "none";
+}
+
+function openRegisterForm() {
+  image1.style.display = "none";
+  image2.style.display = "block";
+  toggleBtn1.style.display = "none";
+  toggleBtn.style.display = "block";
+  loginForm.style.display = "none";
+  registerForm.style.display = "block";
+}
+
+registerForm.onsubmit = function (event) {
+  // Prevent the form from refreshing the page,
+  // as it will do by default when the Submit event is triggered:
+  event.preventDefault();
+
+  // We can use loginForm.username (for example) to access
+  // the input element in the form which has the ID of "username".
+  const registerData = {
+    username: registerForm.registerUsername.value,
+    password: registerForm.registerPassword.value,
+    fullName: registerForm.registerFullName.value,
+  };
+
+  // Disables the button after the form has been submitted already:
+  registerForm.registerButton.disabled = true;
+
+  // Time to actually process the login using the function from auth.js!
+  register(registerData);
 };
