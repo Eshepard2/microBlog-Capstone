d49ee8ff2ea17a1816ef74eac19e91d636796abc
Charonn <charonn.edwards412@gmail.com>
2022-12-22 11:08:56 -0500

    style navbar, add stories html to posts/index.html

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,18 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+let allPostsEndpoint = "https://microbloglite.herokuapp.com/api/posts"
+
+fetch(allPostsEndpoint)
+            .then(response => response.json())
+            .then(data => {
+                for (let i = 0; i < data.length; i++) {
+                    let row = table.insertRow(-1);
+                    let cell1 = row.insertCell(0)
+                    let cell2 = row.insertCell(1)
+                    cell1.innerHTML = data[i].username
+                    cell2.innerHTML = data[i].text
+
+                }
+            });
