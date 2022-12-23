a80d1400ace402043e7c5ec18c7a971e33b4783a
Eshepard2 <Eshepard2@outlook.com>
2022-12-23 13:13:50 -0500

    delete legend, move script to bottom of page, delcared variables on profile.js

---
+++ profilePage/profile.js
@@ -0,0 +1,8 @@
+
+// get all DOM Nodes 
+
+const newPostForm = document.querySelector("form");
+const newPostTextArea = document.querySelector("textarea");
+const newPostBtn = document.querySelector("newPostBtn");
+
+
