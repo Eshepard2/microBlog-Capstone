ef81505f8f3f1f6eae46dc358335b92206dbde03
Charonn <charonn.edwards412@gmail.com>
2022-12-30 18:09:09 -0500

    add view all posts button, write function to display all posts, write function to display first 10 posts, add logout button and write eventlistener on button, working on main tag CSS

--- posts/posts.js
+++ posts/posts.js
@@ -1,13 +1,101 @@
 /* Posts Page JavaScript */
 
 "use strict";
+// all variablies
+const allPostsEndpoint = "https://microbloglite.herokuapp.com/api/posts";
+const pElement = document.getElementById("postText");
+const viewAllBtn = document.getElementById("viewAll");
+let logoutBtn = document.getElementById("logout");
 
-let allPostsEndpoint = "https://microbloglite.herokuapp.com/api/posts";
-let pElement = document.getElementById("postText");
+//logut function
+logoutBtn.addEventListener("click", logout)
 
-fetch(allPostsEndpoint)
-  .then((response) => response.json())
-  .then((data) => {
-    let message = `<li> User: ${data.username} </li> <li> Post: ${data.text} </li>`;
-    pElement.innerHTML = message;
-  });
+//View All Button, to see all posts
+viewAllBtn.addEventListener("click", getAllUsersPosts)
+
+//calling function before the rest of the JavaScript is executed
+getUsersPosts();
+
+
+function getAllUsersPosts() {
+  const loginData = getLoginData();
+  console.log(loginData.token);
+  let options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${loginData.token}`,
+    },
+  };
+  fetch(allPostsEndpoint, options)
+    .then((response) => response.json())
+    .then((data) => toHTML(data));
+}
+
+
+function toHTML(data) {
+    data.forEach((e) => {
+      pElement.innerHTML += `<article>
+   <b>${e.username}</b>
+   <p>${e.text}</p>
+   </article>
+  `;
+    });
+  }
+
+
+
+
+function getUsersPosts() {
+    const loginData = getLoginData();
+    let options = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+        },
+    };
+    fetch(allPostsEndpoint + `/?limit=10&offset=1`, options)
+    .then((response) => response.json())
+    .then((data) => toHTML(data));
+}
+
+
+function searchBarFunction() {
+  const searchBar = document.getElementById("search")
+  const filter = searchBar.value.toLowerCase()
+
+  
+}
+
+
+
+// function getUsersPosts() {
+//   const loginData = getLoginData();
+//   let options = {
+//       method: "GET",
+//       headers: {
+//           Authorization: `Bearer ${loginData.token}`,
+//       },
+//   };
+//   fetch(allPostsEndpoint + `/?limit=20&offset=3&username=${userName}`, options)
+//       .then(response => response.json())
+//       .then(posts => { console.log(posts) })
+// }
+
+// pElement.innerHTML = data
+
+// function toHTML(data) {
+//   data.forEach((e) => {
+//     pElement.innerHTML += `<article>
+//  <p>${e.username}</p>
+//  <p>${e.text}</p>
+//  </article>
+// `;
+//   });
+// }
+
+// for loop
+
+//   fetch(allPostsEndpoint + `/api/posts/?limit=20&offset=0&username=${userName}`, options)
+//         .then(response => response.json())
+//         .then(data => { let message = `<li> User: ${data.username} </li> <li> Post: ${data.text} </li>`;
+//             pElement.innerHTML = message; })
