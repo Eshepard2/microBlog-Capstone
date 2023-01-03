3d0d42d264ae9a97f06f26de8e04382affae5564
Eshepard2 <Eshepard2@outlook.com>
2023-01-03 10:04:53 -0500

    create function, create variable for (newPostForm,newPostTextArea,value) create const for loginData, create variable options and assigned( method, headers, body of function), create fetch method, declared a variable for endpoint, add then method, add catch method,assigned variable to location posts

--- profilePage/profile.js
+++ profilePage/profile.js
@@ -1,8 +1,40 @@
 
 // get all DOM Nodes 
+let createPostEndpoint = "https://microbloglite.herokuapp.com/api/posts";
+const logOutBtn = document.getElementById("logOutBtn");
+const newPostTextArea = document.querySelector("textarea").value;
+const newPostBtn = document.getElementById("newPostBtn");
 
-const newPostForm = document.querySelector("form");
-const newPostTextArea = document.querySelector("textarea");
-const newPostBtn = document.querySelector("newPostBtn");
+
+// add event listener
+newPostBtn.addEventListener("click",postSomething);
+logOutBtn.addEventListener("click", logout);
+
+
+function postSomething() {
+let text = newPostForm.newPostTextArea.value
+const loginData = getLoginData();
+  console.log(loginData.token);
+  let options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Authorization": `Bearer ${loginData.token}`
+    },
+    body: JSON.stringify({
+        "text": text,
+    })
+  };
+
+fetch(createPostEndpoint, options)
+    .then((response) => response.json())
+    .then((post) => console.log(post))
+    .catch((error) => console.error(error));
+    window.location.assign("/posts")
+
+}
+
+    // .catch(error => console.error(error))
+
 
 
